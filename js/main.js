1a7f409dd050df5f60e9b1851dce6b47c9d460a4
vtnboolean <vnthuanx@gmail.com>
2022-06-15 18:16:45 +0200

    select menu age done

--- js/main.js
+++ js/main.js
@@ -3,14 +3,15 @@ const generatorButton = document.querySelector('#generator-button');
 generatorButton.addEventListener('click', function(){
     console.log(document.getElementById('input-name').value);
     console.log(document.getElementById('input-distance').value);
-    console.log(document.getElementById('input-age').value);
+    console.log(document.getElementById('input-age-rank').value);
     const userDistance = document.getElementById('input-distance').value;
-    const userAge = document.getElementById('input-age').value;
+    const userAge = document.getElementById('input-age-rank').value;
     const userName = document.getElementById('input-name').value;
     document.querySelector('#output-nome').innerHTML= userName;
+    
     let price = (userDistance * 0.26);
 
-    if (userAge < 18){
+    if (userAge == "minor"){
         // Calcolo con lo sconto del 15% per minorenni
         price = price - ((price * 15) / 100);
         price=(Math.round(price * 100)/100).toFixed(2);
@@ -19,8 +20,8 @@ generatorButton.addEventListener('click', function(){
         document.querySelector('#output-costo').innerHTML= (price + "€" );
         document.querySelector('#output-carrozza').innerHTML= Math.floor(Math.random() * 10) + 1;
         document.querySelector('#output-codice').innerHTML= Math.floor(Math.random() * 99999) + 1;
-        // Calcolo con lo sconto del 40% per over65
-    } else if (userAge >= 65) {
+        // Calcolo con lo sconto del 35% per over65
+    } else if (userAge == "senior") {
         price= price - ((price * 35) / 100);
         price=(Math.round(price * 100)/100).toFixed(2);
         console.log(`Il prezzo del biglietto scontato del 35% è ${price}€`);
