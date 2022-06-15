89bc71a98bc787995e2c71aa3141cd9620bc0dbe
vtnboolean <vnthuanx@gmail.com>
2022-06-15 16:30:04 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const generatorButton = document.querySelector('#generator-button');
+
+generatorButton.addEventListener('click', function(){
+    console.log(document.getElementById('input-name').value);
+    console.log(document.getElementById('input-distance').value);
+    console.log(document.getElementById('input-age').value);
+    const userDistance = document.getElementById('input-distance').value;
+    const userAge = document.getElementById('input-age').value;
+
+
+
+    let price = (userDistance * 0.26);
+
+    if (userAge < 18){
+        // Calcolo con lo sconto del 15% per minorenni
+        price = price - ((price * 15) / 100);
+        price=(Math.round(price * 100)/100).toFixed(2);
+        console.log(`Il prezzo del biglietto scontato del 20% è ${price}`);
+        // Calcolo con lo sconto del 40% per over65
+    } else if (userAge >= 65) {
+        price= price - ((price * 35) / 100);
+        price=(Math.round(price * 100)/100).toFixed(2);
+        console.log(`Il prezzo del biglietto scontato del 35% è ${price65Plus}`);
+        // Prezzo standard
+
+    } else {
+        price=(Math.round(price * 100)/100).toFixed(2);
+        console.log(`Il prezzo del biglietto standard è ${price}`);
+    }
+     
+    
+    console.log(`Il prezzo del biglietto è ${price}`);
+});
+    
+
+           
+
